0f7c2f86597356ee64fad69f99d23f334cd857b9
JinHoooooou <jinho4744@naver.com>
2020-04-14 11:45:56 +0900

    2020-04-14-codewars-solution

--- src/sumOfOddNumbers_20200412/RowSumOddNumbers.java
+++ src/sumOfOddNumbers_20200412/RowSumOddNumbers.java
@@ -3,24 +3,6 @@ package sumOfOddNumbers_20200412;
 public class RowSumOddNumbers {
 
   public static int rowSumOddNumbers(int n) {
-    int startIndex = 0;
-    int endIndex = 0;
-    for (int i = 0; i <= n; i++) {
-      endIndex += i;
-    }
-    startIndex = endIndex - n;
-    int[] oddArray = new int[endIndex];
-    int odd = 1;
-    for (int i = 0; i < endIndex; i++) {
-      oddArray[i] = odd;
-      odd += 2;
-    }
-    int sum = 0;
-    for (int i = startIndex; i < endIndex; i++) {
-      sum += oddArray[i];
-    }
-
-    return sum;
+    return n * n * n;
   }
-
 }

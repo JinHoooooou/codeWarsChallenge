cf4f478d6d61f291777fbd1cc3beee3884d1f0f2
JinHoooooou <jinho4744@naver.com>
2020-04-28 10:47:26 +0900

    2020-04-28-codewars-solution

---
+++ src/isNDivisibleByXAndY_20200428/DivisibleNb.java
@@ -0,0 +1,9 @@
+package isNDivisibleByXAndY_20200428;
+
+public class DivisibleNb {
+
+  public static boolean isDivisible(long n, long x, long y) {
+    return n % x == 0 && n % y == 0;
+  }
+
+}
---
+++ src/jennySecretMessage_20200428/Greeter.java
@@ -0,0 +1,12 @@
+package jennySecretMessage_20200428;
+
+public class Greeter {
+
+  public static String greet(String name) {
+    if (name.equals("Johnny")) {
+      return "Hello, my love!";
+    }
+    return String.format("Hello, %s!", name);
+  }
+}
+

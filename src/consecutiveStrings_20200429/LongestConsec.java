71215bab7a94bc5dc864a40db2a6da21b4ccb29d
JinHoooooou <jinho4744@naver.com>
2020-04-29 14:34:27 +0900

    2020-04-29-codewars-solution

---
+++ src/consecutiveStrings_20200429/LongestConsec.java
@@ -0,0 +1,37 @@
+package consecutiveStrings_20200429;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+public class LongestConsec {
+
+  public static String longestConsec(String[] strarr, int k) {
+
+    if (k > strarr.length || k <= 0) {
+      return "";
+    }
+    List<String> consecutiveStringList = makeConsecutiveString(strarr, k);
+
+    String result = consecutiveStringList.get(0);
+    for (String consecutiveString : consecutiveStringList) {
+      if (result.length() < consecutiveString.length()) {
+        result = consecutiveString;
+      }
+    }
+    return result;
+  }
+
+  private static List<String> makeConsecutiveString(String[] strarr, int k) {
+    List<String> list = new ArrayList<>();
+    for (int i = 0; i < strarr.length - k + 1; i++) {
+      String element = "";
+      for (int j = i; j < k + i; j++) {
+        element += strarr[j];
+      }
+      list.add(i, element);
+    }
+    return list;
+  }
+}

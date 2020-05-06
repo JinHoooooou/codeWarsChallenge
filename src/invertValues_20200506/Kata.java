4634e5554fb5fc2c4fcefb87be927b02c4af20e2
JinHoooooou <jinho4744@naver.com>
2020-05-06 11:42:08 +0900

    2020-05-06-codewars-solution

---
+++ src/invertValues_20200506/Kata.java
@@ -0,0 +1,10 @@
+package invertValues_20200506;
+
+import java.util.Arrays;
+
+public class Kata {
+
+  public static int[] invert(int[] array) {
+    return Arrays.stream(array).map(i -> i * -1).toArray();
+  }
+}

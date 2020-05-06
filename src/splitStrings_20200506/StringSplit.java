48e45cffb3214af98d937e9582953789e79c2a33
JinHoooooou <jinho4744@naver.com>
2020-05-06 11:47:18 +0900

    2020-05-06-StringSplit-Add-Test

---
+++ src/splitStrings_20200506/StringSplit.java
@@ -0,0 +1,9 @@
+package splitStrings_20200506;
+
+public class StringSplit {
+
+  public static String[] solution(String s) {
+
+    return new String[]{};
+  }
+}
---
+++ src/splitStrings_20200506/StringSplitTest.java
@@ -0,0 +1,13 @@
+package splitStrings_20200506;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class StringSplitTest {
+
+  @Test
+  public void testShouldFail() {
+    fail();
+  }
+}

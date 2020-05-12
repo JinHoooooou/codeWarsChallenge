4dafa3631561d25d2ac0d10ef94ee46d4f8d66b3
JinHoooooou <jinho4744@naver.com>
2020-05-12 11:41:31 +0900

    2020-05-12-Alternating-Split-add-actual-code-for-passing-decrypt-method-first-test

--- src/alternatingSplit_20200512/Kata.java
+++ src/alternatingSplit_20200512/Kata.java
@@ -22,6 +22,21 @@ public class Kata {
   }
 
   public static String decrypt(final String encryptedText, final int n) {
-    return "";
+    if (encryptedText == null || n < 1) {
+      return encryptedText;
+    }
+    String tempText = encryptedText;
+    String result = "";
+    for (int i = 0; i < n; i++) {
+      result = "";
+      for (int j = 0, k = tempText.length() / 2; j < tempText.length() / 2; j++, k++) {
+        result += "" + tempText.charAt(k) + tempText.charAt(j);
+      }
+      if (tempText.length() % 2 == 1) {
+        result += "" + tempText.charAt(tempText.length() - 1);
+      }
+      tempText = result;
+    }
+    return result;
   }
 }

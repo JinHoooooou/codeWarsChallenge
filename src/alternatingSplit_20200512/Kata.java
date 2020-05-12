ea138125d82b12ffc27f5308b08afd25490ee868
JinHoooooou <jinho4744@naver.com>
2020-05-12 10:30:44 +0900

    2020-05-12-Alternating-Split-add-actual-code-for-passing-encrypt-method-test

--- src/alternatingSplit_20200512/Kata.java
+++ src/alternatingSplit_20200512/Kata.java
@@ -3,7 +3,19 @@ package alternatingSplit_20200512;
 public class Kata {
 
   public static String encrypt(final String text, final int n) {
-    return "";
+
+    String tempText = text;
+    String result = "";
+    for (int i = 0; i < n; i++) {
+      for (int j = 1; j < tempText.length(); j += 2) {
+        result += "" + tempText.charAt(j);
+      }
+      for (int j = 0; j < tempText.length(); j += 2) {
+        result += "" + tempText.charAt(j);
+      }
+      tempText = result;
+    }
+    return result;
   }
 
   public static String decrypt(final String encryptedText, final int n) {

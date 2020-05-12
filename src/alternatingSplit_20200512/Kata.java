27215325adffae604d73119aab0688ec8a1c25ce
JinHoooooou <jinho4744@naver.com>
2020-05-12 10:14:49 +0900

    2020-05-11-Alternating-Split-add-first-encrypt-test

---
+++ src/alternatingSplit_20200512/Kata.java
@@ -0,0 +1,12 @@
+package alternatingSplit_20200512;
+
+public class Kata {
+
+  public static String encrypt(final String text, final int n) {
+    return "";
+  }
+
+  public static String decrypt(final String encryptedText, final int n) {
+    return "";
+  }
+}
---
+++ src/alternatingSplit_20200512/KataTest.java
@@ -0,0 +1,21 @@
+package alternatingSplit_20200512;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class KataTest {
+
+  @Test
+  @DisplayName("test should return hsi  etTi sats! when input string This is a test!, 1")
+  public void test1() {
+    // Given: Set string and n times
+    String givenString = "This is a test!";
+    int givenTimes = 1;
+    // When: Call encrypt method
+    String actual = Kata.encrypt(givenString, givenTimes);
+    // Then: should return "hsi  etTi sats!"
+    assertEquals("hsi  etTi sats!", actual);
+  }
+}

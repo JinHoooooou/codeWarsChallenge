2d7bc7ea8bc69bc05aba068b406504dfbb363866
JinHoooooou <jinho4744@naver.com>
2020-05-14 14:16:08 +0900

    2020-05-14-Highest-Scoring-Word-refactor-actual-code-for-clean-code

--- src/highestScoringWord_20200514/Kata.java
+++ src/highestScoringWord_20200514/Kata.java
@@ -3,19 +3,23 @@ package highestScoringWord_20200514;
 public class Kata {
 
   public static String high(String s) {
-    String[] split = s.split(" ");
-    int max = 0;
+    int highest = 0;
     String result = "";
-    for (String word : split) {
-      int wordScore = 0;
-      for (int i = 0; i < word.length(); i++) {
-        wordScore += word.charAt(i) - 96;
-      }
-      if (wordScore > max) {
-        max = wordScore;
+    for (String word : s.split(" ")) {
+      int wordScore = getWordScore(word);
+      if (wordScore > highest) {
+        highest = wordScore;
         result = word;
       }
     }
     return result;
+  }
+
+  private static int getWordScore(String word) {
+    int wordScore = 0;
+    for (int i = 0; i < word.length(); i++) {
+      wordScore += word.charAt(i) - 96;
+    }
+    return wordScore;
   }
 }

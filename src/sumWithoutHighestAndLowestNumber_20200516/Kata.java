be4793e1147461ecd03f6ca5cfa1b31d3a4bca6f
JinHoooooou <jinho4744@naver.com>
2020-05-16 13:53:07 +0900

    2020-05-16-Sum-Without-Highest-And-Lowest-Number-add-actual-code-for-passing-second-test

--- src/sumWithoutHighestAndLowestNumber_20200516/Kata.java
+++ src/sumWithoutHighestAndLowestNumber_20200516/Kata.java
@@ -5,7 +5,9 @@ import java.util.stream.IntStream;
 public class Kata {
 
   public static int sum(int[] numbers) {
-
+    if (numbers == null || numbers.length <= 1) {
+      return 0;
+    }
     int max = IntStream.of(numbers).max().getAsInt();
     int min = IntStream.of(numbers).min().getAsInt();
 

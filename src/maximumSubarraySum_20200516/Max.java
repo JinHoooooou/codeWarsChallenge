501b3fb150fec9405af54d8a6d45823e3ae95eae
JinHoooooou <jinho4744@naver.com>
2020-05-16 14:16:16 +0900

    2020-05-16-Maximum-Subarray-Sum-add-actual-code-for-passing-second-test

--- src/maximumSubarraySum_20200516/Max.java
+++ src/maximumSubarraySum_20200516/Max.java
@@ -1,8 +1,13 @@
 package maximumSubarraySum_20200516;
 
+import java.util.stream.IntStream;
+
 public class Max {
 
   public static int sequence(int[] arr) {
-    return arr.length == 0 ? 0 : 1;
+    if (arr.length == 0) {
+      return 0;
+    }
+    return IntStream.of(arr).sum();
   }
 }

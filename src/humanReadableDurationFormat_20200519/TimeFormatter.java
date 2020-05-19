95a79ad09d95a0f7479fb683c70a92e2d52c3c90
JinHoooooou <jinho4744@naver.com>
2020-05-19 21:04:37 +0900

    2020-05-19-Human-Readable-Duration-Format-refactor-for-passing-all-test

--- src/humanReadableDurationFormat_20200519/TimeFormatter.java
+++ src/humanReadableDurationFormat_20200519/TimeFormatter.java
@@ -1,5 +1,8 @@
 package humanReadableDurationFormat_20200519;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class TimeFormatter {
 
   public static String formatDuration(int seconds) {
@@ -7,34 +10,36 @@ public class TimeFormatter {
       return "now";
     }
     String result = "";
-    int day = seconds / (3600 * 24);
+    List<String> format = new ArrayList<>();
+    int year = seconds / (3600 * 24 * 365);
+    int day = seconds % (3600 * 24 * 365) / (3600 * 24);
     int hour = seconds % (3600 * 24) / 3600;
     int minute = seconds % 3600 / 60;
     int second = seconds % 60;
 
-    if (second > 0) {
-      result = second + (second > 1 ? " seconds" : " second");
+    if (year > 0) {
+      format.add(year + (year > 1 ? " years" : " year"));
     }
-    if (minute > 0) {
-      if (second > 0) {
-        result = " and " + result;
-      }
-      result = minute + (minute > 1 ? " minutes" : " minute") + result;
+    if (day > 0) {
+      format.add(day + (day > 1 ? " days" : " day"));
     }
     if (hour > 0) {
-      if (minute > 0 || second > 0) {
-        result = ", " + result;
-      }
-      result = hour + (hour > 1 ? " hours" : " hour") + result;
+      format.add(hour + (hour > 1 ? " hours" : " hour"));
     }
-
-    if (day > 0) {
-      if (hour > 0 || minute > 0 || second > 0) {
-        result = ", " + result;
-      }
-      result = day + (day > 1 ? " days" : " day") + result;
+    if (minute > 0) {
+      format.add(minute + (minute > 1 ? " minutes" : " minute"));
     }
-
+    if (second > 0) {
+      format.add(second + (second > 1 ? " seconds" : " second"));
+    }
+    if (format.size() > 1) {
+      result = String.join(", ", format);
+      String frontComponent = result.substring(0, result.lastIndexOf(","));
+      String lastComponent = result.substring(result.lastIndexOf(",") + 1);
+      result = frontComponent + " and" + lastComponent;
+    } else {
+      result = String.join("", format);
+    }
     return result;
   }
 }

68828b8213172988fb49fbe62600649444f5e455
JinHoooooou <jinho4744@naver.com>
2020-05-19 19:31:55 +0900

    2020-05-19-Human-Readable-Duration-Format-add-actual-code-for-passing-second-test

--- src/humanReadableDurationFormat_20200519/TimeFormatter.java
+++ src/humanReadableDurationFormat_20200519/TimeFormatter.java
@@ -3,6 +3,20 @@ package humanReadableDurationFormat_20200519;
 public class TimeFormatter {
 
   public static String formatDuration(int seconds) {
-    return seconds + " second";
+    String result = "";
+    if (seconds >= 60) {
+      if (seconds / 60 > 1) {
+        result = seconds / 60 + " minutes and ";
+      } else {
+        result = seconds / 60 + " minute and ";
+      }
+    }
+    if (seconds % 60 > 1) {
+      result = result + seconds % 60 + " seconds";
+    } else {
+      result = result + seconds % 60 + " second";
+    }
+
+    return result;
   }
 }

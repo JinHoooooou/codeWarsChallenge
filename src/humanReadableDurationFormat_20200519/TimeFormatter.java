75afba540d649d46295160ab8c65dcd30ec6e4c3
JinHoooooou <jinho4744@naver.com>
2020-05-19 20:12:41 +0900

    2020-05-19-Human-Readable-Duration-Format-add-actual-code-for-passing-fourth-test

--- src/humanReadableDurationFormat_20200519/TimeFormatter.java
+++ src/humanReadableDurationFormat_20200519/TimeFormatter.java
@@ -4,8 +4,10 @@ public class TimeFormatter {
 
   public static String formatDuration(int seconds) {
     String result = "";
+    int hour = seconds / 3600;
+    int minute = seconds % 3600 / 60;
     int second = seconds % 60;
-    int minute = seconds / 60;
+
     if (second > 0) {
       result = second + (second > 1 ? " seconds" : " second");
     }
@@ -14,6 +16,12 @@ public class TimeFormatter {
         result = " and " + result;
       }
       result = minute + (minute > 1 ? " minutes" : " minute") + result;
+    }
+    if (hour > 0) {
+      if (minute > 0 || second > 0) {
+        result = " and" + result;
+      }
+      result = hour + (hour > 1 ? " hours" : " hour") + result;
     }
 
     return result;

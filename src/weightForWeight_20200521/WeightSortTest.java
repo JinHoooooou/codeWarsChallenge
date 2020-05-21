2d9793054339a902ae742f3ac305598323ee8b39
JinHoooooou <jinho4744@naver.com>
2020-05-21 14:57:33 +0900

    2020-05-21-Weight-For-Weight-add-second-test

--- src/weightForWeight_20200521/WeightSortTest.java
+++ src/weightForWeight_20200521/WeightSortTest.java
@@ -16,4 +16,15 @@ class WeightSortTest {
     // Then: should return 2000 103 123 4444 99
     assertEquals("2000 103 123 4444 99", WeightSort.orderWeight(given));
   }
+
+  @Test
+  @DisplayName("test should return 11 11 2000 10003 22 123 1234000 44444444 9999"
+      + " when input 2000 10003 1234000 44444444 9999 11 11 22 123")
+  public void test2() {
+    // Given: Set weight list
+    String given = "2000 10003 1234000 44444444 9999 11 11 22 123";
+
+    // Then: should return 2000 103 123 4444 99
+    assertEquals("11 11 2000 10003 22 123 1234000 44444444 9999", WeightSort.orderWeight(given));
+  }
 }

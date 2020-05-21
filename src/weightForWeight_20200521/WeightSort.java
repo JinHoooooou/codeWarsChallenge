6d1e6215dd1498941eaa0fe0ab5ddfa081d9a76d
JinHoooooou <jinho4744@naver.com>
2020-05-21 15:17:00 +0900

    2020-05-21-Weight-For-Weight-add-actual-code-for-passing-second-test

--- src/weightForWeight_20200521/WeightSort.java
+++ src/weightForWeight_20200521/WeightSort.java
@@ -1,28 +1,39 @@
 package weightForWeight_20200521;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Map;
 import java.util.TreeMap;
 
 public class WeightSort {
 
   public static String orderWeight(String string) {
-    Map<Integer, String> map = getWeightMap(string);
+    Map<Integer, ArrayList<String>> map = getWeightMap(string);
     return getResultString(map);
   }
 
-  private static String getResultString(Map<Integer, String> map) {
+  private static String getResultString(Map<Integer, ArrayList<String>> map) {
     String resultString = "";
     for (Integer key : map.keySet()) {
-      resultString += map.get(key) + " ";
+      ArrayList<String> weightList = map.get(key);
+      Collections.sort(weightList);
+      for (String weight : weightList) {
+        resultString += weight + " ";
+      }
     }
     return resultString.trim();
   }
 
-  private static Map<Integer, String> getWeightMap(String string) {
-    Map<Integer, String> map = new TreeMap<>();
+  private static Map<Integer, ArrayList<String>> getWeightMap(String string) {
+    Map<Integer, ArrayList<String>> map = new TreeMap<>();
     for (String weight : string.split(" ")) {
+      ArrayList<String> valueList = new ArrayList<>();
       int key = getWeightPoint(weight);
-      map.put(key, weight);
+      if (map.containsKey(key)) {
+        valueList = map.get(key);
+      }
+      valueList.add(weight);
+      map.put(key, valueList);
     }
     return map;
   }

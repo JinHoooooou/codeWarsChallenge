c04c564f5783236f8066f091c17d667883136875
JinHoooooou <jinho4744@naver.com>
2020-05-23 13:14:38 +0900

    2020-05-23-Reduce-But-Grow-add-actual-code-for-passing-first-test

--- src/reduceButGrow_20200523/Kata.java
+++ src/reduceButGrow_20200523/Kata.java
@@ -1,5 +1,10 @@
 package reduceButGrow_20200523;
 
+import java.util.stream.IntStream;
+
 public class Kata {
 
+  public static int grow(int[] x) {
+    return IntStream.of(x).reduce((a, b) -> a *= b).getAsInt();
+  }
 }

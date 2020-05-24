0f38d611b365d40ca54fe6af35acc0b7b4e8aa0a
JinHoooooou <jinho4744@naver.com>
2020-05-24 14:20:42 +0900

    2020-05-24-Snail-add-actual-code-for-passing-second-test

--- src/snail_20200524/Snail.java
+++ src/snail_20200524/Snail.java
@@ -3,6 +3,15 @@ package snail_20200524;
 public class Snail {
 
   public static int[] snail(int[][] array) {
-    return array.length == 1 ? new int[]{1} : null;
+    if (array.length == 1) {
+      return new int[]{1};
+    }
+    int[] result = new int[array.length * array.length];
+    result[0] = array[0][0];
+    result[1] = array[0][1];
+    result[2] = array[1][1];
+    result[3] = array[1][0];
+
+    return result;
   }
 }

bc58e8b516a110a19af730c46e172d33ac59fea9
JinHoooooou <jinho4744@naver.com>
2020-05-24 14:24:57 +0900

    2020-05-24-Snail-add-actual-code-for-passing-third-test

--- src/snail_20200524/Snail.java
+++ src/snail_20200524/Snail.java
@@ -7,10 +7,24 @@ public class Snail {
       return new int[]{1};
     }
     int[] result = new int[array.length * array.length];
-    result[0] = array[0][0];
-    result[1] = array[0][1];
-    result[2] = array[1][1];
-    result[3] = array[1][0];
+    if (array.length == 2) {
+      result[0] = array[0][0];
+      result[1] = array[0][1];
+      result[2] = array[1][1];
+      result[3] = array[1][0];
+    } else if (array.length == 3) {
+      result[0] = array[0][0];
+      result[1] = array[0][1];
+      result[2] = array[0][2];
+
+      result[3] = array[1][2];
+      result[4] = array[2][2];
+
+      result[5] = array[2][1];
+      result[6] = array[2][0];
+      result[7] = array[1][0];
+      result[8] = array[1][1];
+    }
 
     return result;
   }

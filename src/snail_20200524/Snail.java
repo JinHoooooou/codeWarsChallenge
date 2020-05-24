1501208472daa249d3f8dbf24c37d47e2b84991d
JinHoooooou <jinho4744@naver.com>
2020-05-24 14:51:25 +0900

    2020-05-24-Snail-add-actual-code-for-passing-fourth-test

--- src/snail_20200524/Snail.java
+++ src/snail_20200524/Snail.java
@@ -39,6 +39,26 @@ public class Snail {
         result[index] = array[1][i];
         index++;
       }
+    } else if (array.length == 4) {
+      int index = 0;
+      for (int i = 0; i < 2; i++) {
+        for (int j = i; j < array.length - i; j++) {
+          result[index] = array[i][j];
+          index++;
+        }
+        for (int j = i + 1; j < array.length - i; j++) {
+          result[index] = array[j][array.length - 1 - i];
+          index++;
+        }
+        for (int j = array.length - 2 - i; j >= i; j--) {
+          result[index] = array[array.length - 1 - i][j];
+          index++;
+        }
+        for (int j = array.length - 2 - i; j >= i + 1; j--) {
+          result[index] = array[j][i];
+          index++;
+        }
+      }
     }
 
     return result;

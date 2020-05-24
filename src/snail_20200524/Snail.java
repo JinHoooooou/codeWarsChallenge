0b0d225a23727bfac859a34a34224c10dedd5d8b
JinHoooooou <jinho4744@naver.com>
2020-05-24 15:07:23 +0900

    2020-05-24-Snail-merge-all-case-for-clean-code

--- src/snail_20200524/Snail.java
+++ src/snail_20200524/Snail.java
@@ -3,52 +3,30 @@ package snail_20200524;
 public class Snail {
 
   public static int[] snail(int[][] array) {
-    if (array.length == 1) {
-      return new int[]{1};
-    }
     int[] result = new int[array.length * array.length];
     int index = 0;
-    if (array.length % 2 == 0) {
-      for (int i = 0; i < array.length / 2; i++) {
-        for (int j = i; j < array.length - i; j++) {
-          result[index] = array[i][j];
-          index++;
-        }
-        for (int j = i + 1; j < array.length - i; j++) {
-          result[index] = array[j][array.length - 1 - i];
-          index++;
-        }
-        for (int j = array.length - 2 - i; j >= i; j--) {
-          result[index] = array[array.length - 1 - i][j];
-          index++;
-        }
-        for (int j = array.length - 2 - i; j >= i + 1; j--) {
-          result[index] = array[j][i];
-          index++;
-        }
+    for (int i = 0; i < array.length / 2; i++) {
+      for (int j = i; j < array.length - i; j++) {
+        result[index] = array[i][j];
+        index++;
       }
-    } else {
-      for (int i = 0; i < array.length / 2; i++) {
-        for (int j = i; j < array.length - i; j++) {
-          result[index] = array[i][j];
-          index++;
-        }
-        for (int j = i + 1; j < array.length - i; j++) {
-          result[index] = array[j][array.length - 1 - i];
-          index++;
-        }
-        for (int j = array.length - 2 - i; j >= i; j--) {
-          result[index] = array[array.length - 1 - i][j];
-          index++;
-        }
-        for (int j = array.length - 2 - i; j >= i + 1; j--) {
-          result[index] = array[j][i];
-          index++;
-        }
-        result[index] = array[array.length / 2][array.length / 2];
+      for (int j = i + 1; j < array.length - i; j++) {
+        result[index] = array[j][array.length - 1 - i];
+        index++;
+      }
+      for (int j = array.length - 2 - i; j >= i; j--) {
+        result[index] = array[array.length - 1 - i][j];
+        index++;
+      }
+      for (int j = array.length - 2 - i; j >= i + 1; j--) {
+        result[index] = array[j][i];
+        index++;
       }
     }
 
+    if (array.length % 2 == 1) {
+      result[index] = array[array.length / 2][array.length / 2];
+    }
     return result;
   }
 }

acd2da6d20117dd9ece9f5031701b6cbb97d4be6
JinHoooooou <jinho4744@naver.com>
2020-05-24 14:50:47 +0900

    2020-05-24-Snail-add-fourth-test

--- src/snail_20200524/SnailTest.java
+++ src/snail_20200524/SnailTest.java
@@ -50,4 +50,21 @@ class SnailTest {
     // Then: Should return {1,2,3,6,9,8,7,4,5}
     assertArrayEquals(new int[]{1, 2, 3, 6, 9, 8, 7, 4, 5}, actual);
   }
+
+  @Test
+  @DisplayName("test when array length 4")
+  public void test4() {
+    // Given: set 2d array
+    int[][] given = {
+        {1, 2, 3, 4},
+        {5, 6, 7, 8},
+        {9, 10, 11, 12},
+        {13, 14, 15, 16}};
+
+    // When: Call snail method
+    int[] actual = Snail.snail(given);
+
+    // Then: Should return {1,2,3,4,8,12,16,15,14,13,9,5,6,7,11,10}
+    assertArrayEquals(new int[]{1, 2, 3, 4, 8, 12, 16, 15, 14, 13, 9, 5, 6, 7, 11, 10}, actual);
+  }
 }

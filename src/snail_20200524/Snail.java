5dc99563001c6a9115f21a6b17da23a8b8b57017
JinHoooooou <jinho4744@naver.com>
2020-05-24 15:05:37 +0900

    2020-05-24-Snail-add-actual-code-for-passing-fifth-test

--- src/snail_20200524/Snail.java
+++ src/snail_20200524/Snail.java
@@ -9,7 +9,7 @@ public class Snail {
     int[] result = new int[array.length * array.length];
     int index = 0;
     if (array.length % 2 == 0) {
-      for (int i = 0; i < array.length; i++) {
+      for (int i = 0; i < array.length / 2; i++) {
         for (int j = i; j < array.length - i; j++) {
           result[index] = array[i][j];
           index++;
@@ -27,22 +27,25 @@ public class Snail {
           index++;
         }
       }
-    } else if (array.length == 3) {
-      for (int i = 0; i < array.length; i++) {
-        result[index] = array[0][i];
-        index++;
-      }
-      for (int i = 1; i < array.length; i++) {
-        result[index] = array[i][array.length - 1];
-        index++;
-      }
-      for (int i = 1; i >= 0; i--) {
-        result[index] = array[array.length - 1][i];
-        index++;
-      }
-      for (int i = 0; i < 2; i++) {
-        result[index] = array[1][i];
-        index++;
+    } else {
+      for (int i = 0; i < array.length / 2; i++) {
+        for (int j = i; j < array.length - i; j++) {
+          result[index] = array[i][j];
+          index++;
+        }
+        for (int j = i + 1; j < array.length - i; j++) {
+          result[index] = array[j][array.length - 1 - i];
+          index++;
+        }
+        for (int j = array.length - 2 - i; j >= i; j--) {
+          result[index] = array[array.length - 1 - i][j];
+          index++;
+        }
+        for (int j = array.length - 2 - i; j >= i + 1; j--) {
+          result[index] = array[j][i];
+          index++;
+        }
+        result[index] = array[array.length / 2][array.length / 2];
       }
     }
 

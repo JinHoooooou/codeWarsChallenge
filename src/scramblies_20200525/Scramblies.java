f817c8b0f1ee7ff695123ebec1b809076ee0fce1
JinHoooooou <jinho4744@naver.com>
2020-05-25 13:34:35 +0900

    2020-05-25-Scramblies-add-actual-code-for-passing-first-test

--- src/scramblies_20200525/Scramblies.java
+++ src/scramblies_20200525/Scramblies.java
@@ -2,4 +2,14 @@ package scramblies_20200525;
 
 public class Scramblies {
 
+  public static boolean scramble(String str1, String str2) {
+    char[] toArray = str2.toCharArray();
+    for (int i = 0; i < str1.length(); i++) {
+      char target = str1.charAt(i);
+      if (str2.contains(String.valueOf(target))) {
+        str2 = str2.replaceFirst(String.valueOf(target), "");
+      }
+    }
+    return str2.trim().length() == 0;
+  }
 }

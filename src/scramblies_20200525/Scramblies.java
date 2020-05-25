51812bf8ed1c5f433781d0aa46bee0f3a50f28e7
JinHoooooou <jinho4744@naver.com>
2020-05-25 13:45:07 +0900

    2020-05-25-Scramblies-add-actual-code-for-passing-second-test

--- src/scramblies_20200525/Scramblies.java
+++ src/scramblies_20200525/Scramblies.java
@@ -3,8 +3,7 @@ package scramblies_20200525;
 public class Scramblies {
 
   public static boolean scramble(String str1, String str2) {
-    for (int i = 0; i < str1.length(); i++) {
-      String target = String.valueOf(str1.charAt(i));
+    for (String target : str1.split("")) {
       if (str2.contains(target)) {
         str2 = str2.replaceFirst(target, "");
       }

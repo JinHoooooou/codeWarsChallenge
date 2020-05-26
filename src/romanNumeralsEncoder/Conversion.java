07fb096f9be302bc1178f3a20a14d52ba83e35ab
JinHoooooou <jinho4744@naver.com>
2020-05-26 14:32:13 +0900

    2020-05-26-Roman-Numerals-Encoder-add-actual-code-for-passing-third-test

--- src/romanNumeralsEncoder/Conversion.java
+++ src/romanNumeralsEncoder/Conversion.java
@@ -5,7 +5,10 @@ public class Conversion {
   public String solution(int n) {
     String result = "";
     while (n != 0) {
-      if (n >= 4) {
+      if (n >= 5) {
+        result += "V";
+        n -= 5;
+      } else if (n >= 4) {
         result += "IV";
         n -= 4;
       } else {

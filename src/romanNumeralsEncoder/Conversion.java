13258757f4c652af0dc0d8fb5568caff8ca89b7d
JinHoooooou <jinho4744@naver.com>
2020-05-26 14:25:43 +0900

    2020-05-26-Roman-Numerals-Encoder-add-actual-code-for-passing-first-test

--- src/romanNumeralsEncoder/Conversion.java
+++ src/romanNumeralsEncoder/Conversion.java
@@ -2,4 +2,12 @@ package romanNumeralsEncoder;
 
 public class Conversion {
 
+  public String solution(int n) {
+    String result = "";
+    while (n != 0) {
+      result += "I";
+      n -= 1;
+    }
+    return result;
+  }
 }

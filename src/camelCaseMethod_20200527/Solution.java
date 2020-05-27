9dcf066802c039d9cf358d7adfb167ec83caf7ee
JinHoooooou <jinho4744@naver.com>
2020-05-27 16:24:23 +0900

    2020-05-27-Camel-Case-Method-add-actual-code-for-passing-third-test

--- src/camelCaseMethod_20200527/Solution.java
+++ src/camelCaseMethod_20200527/Solution.java
@@ -3,6 +3,9 @@ package camelCaseMethod_20200527;
 public class Solution {
 
   public static String camelCase(String str) {
+    if (str.length() == 0) {
+      return "";
+    }
     String[] words = str.trim().split(" +");
     StringBuilder result = new StringBuilder();
     for (String word : words) {

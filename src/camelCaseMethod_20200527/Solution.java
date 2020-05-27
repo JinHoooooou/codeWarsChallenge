ac19c1cc5fcd238217429bc00cac149712524a06
JinHoooooou <jinho4744@naver.com>
2020-05-27 16:21:39 +0900

    2020-05-27-Camel-Case-Method-add-actual-code-for-passing-second-test

--- src/camelCaseMethod_20200527/Solution.java
+++ src/camelCaseMethod_20200527/Solution.java
@@ -3,7 +3,7 @@ package camelCaseMethod_20200527;
 public class Solution {
 
   public static String camelCase(String str) {
-    String[] words = str.split(" ");
+    String[] words = str.trim().split(" +");
     StringBuilder result = new StringBuilder();
     for (String word : words) {
       word = Character.toUpperCase(word.charAt(0)) + word.substring(1);

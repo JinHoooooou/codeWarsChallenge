5eaf7f71aa0fef8423cf3211c00735581f2b162a
JinHoooooou <jinho4744@naver.com>
2020-05-27 16:18:57 +0900

    2020-05-27-Camel-Case-Method-add-actual-code-for-passing-first-test

--- src/camelCaseMethod_20200527/Solution.java
+++ src/camelCaseMethod_20200527/Solution.java
@@ -2,4 +2,13 @@ package camelCaseMethod_20200527;
 
 public class Solution {
 
+  public static String camelCase(String str) {
+    String[] words = str.split(" ");
+    StringBuilder result = new StringBuilder();
+    for (String word : words) {
+      word = Character.toUpperCase(word.charAt(0)) + word.substring(1);
+      result.append(word);
+    }
+    return result.toString();
+  }
 }

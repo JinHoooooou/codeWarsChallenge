2de59d87aceb8a96466e4d406d0f52062fcb8469
JinHoooooou <jinho4744@naver.com>
2020-05-29 18:10:59 +0900

    2020-05-29-The-Supermarket-Queue-add-actual-code-for-passing-second-test

--- src/theSupermarketQueue_20200529/Solution.java
+++ src/theSupermarketQueue_20200529/Solution.java
@@ -1,10 +1,28 @@
 package theSupermarketQueue_20200529;
 
 import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.Queue;
 
 public class Solution {
 
   public static int solveSuperMarketQueue(int[] customers, int n) {
-    return Arrays.stream(customers).sum();
+    Queue<Integer> queue = new LinkedList<>();
+    for (int customer : customers) {
+      queue.add(customer);
+    }
+    int[] tills = new int[n];
+    int resultTime = 0;
+    while (!queue.isEmpty()) {
+      for (int i = 0; i < tills.length; i++) {
+        if (tills[i] == 0 && !queue.isEmpty()) {
+          tills[i] = queue.poll();
+        }
+      }
+      int min = Arrays.stream(tills).min().getAsInt();
+      tills = Arrays.stream(tills).map(x -> x - min).toArray();
+      resultTime += min;
+    }
+    return resultTime + Arrays.stream(tills).max().getAsInt();
   }
 }

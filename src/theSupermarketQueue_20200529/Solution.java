39c83bed302c6c166a7b0d84aaf430c80b4e6420
JinHoooooou <jinho4744@naver.com>
2020-05-29 16:40:24 +0900

    2020-05-29-The-Supermarket-Queue-add-actual-code-for-passing-first-test

--- src/theSupermarketQueue_20200529/Solution.java
+++ src/theSupermarketQueue_20200529/Solution.java
@@ -1,5 +1,10 @@
 package theSupermarketQueue_20200529;
 
+import java.util.Arrays;
+
 public class Solution {
 
+  public static int solveSuperMarketQueue(int[] customers, int n) {
+    return Arrays.stream(customers).sum();
+  }
 }

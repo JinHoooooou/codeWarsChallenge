71943a43f00f84304d59ab56873fd3ff9433919c
JinHoooooou <jinho4744@naver.com>
2020-05-29 18:10:27 +0900

    2020-05-29-The-Supermarket-Queue-add-second-test

--- src/theSupermarketQueue_20200529/SolutionTest.java
+++ src/theSupermarketQueue_20200529/SolutionTest.java
@@ -9,7 +9,7 @@ class SolutionTest {
 
   @Test
   @DisplayName("test should return sum of array value when input till 1")
-  public void test() {
+  public void test1() {
     // Given: Set input
     int[] queue = {1, 2, 3, 4, 5};
     int tillCount = 1;
@@ -17,4 +17,17 @@ class SolutionTest {
     // Then: should return 15
     assertEquals(15, Solution.solveSuperMarketQueue(queue, tillCount));
   }
+
+  @Test
+  @DisplayName("test should return 9 when input array [2,2,3,3,4,4,] and 2")
+  public void test2() {
+    // Given: Set input
+    int[] queue = {2, 2, 3, 3, 4, 4};
+    int tillCount = 2;
+
+    // Then: should return 9
+    assertEquals(9, Solution.solveSuperMarketQueue(queue, tillCount));
+  }
+
+
 }

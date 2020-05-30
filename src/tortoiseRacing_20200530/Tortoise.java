62d144095a9290e663da3520025f2c7fbc47fcf2
JinHoooooou <jinho4744@naver.com>
2020-05-30 13:48:23 +0900

    2020-05-30-Tortoise-Racing-add-actual-code-for-passing-first-test

--- src/tortoiseRacing_20200530/Tortoise.java
+++ src/tortoiseRacing_20200530/Tortoise.java
@@ -2,4 +2,10 @@ package tortoiseRacing_20200530;
 
 public class Tortoise {
 
+  public static int[] race(int v1, int v2, int g) {
+    if (v1 >= v2) {
+      return null;
+    }
+    return new int[]{};
+  }
 }

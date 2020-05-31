75cbf332b35203d2911b5935c103811d67936644
JinHoooooou <jinho4744@naver.com>
2020-05-31 13:12:33 +0900

    2020-05-31-Range-Extraction-add-actual-code-for-passing-first-test

--- src/rangeExtraction_20200531/Solution.java
+++ src/rangeExtraction_20200531/Solution.java
@@ -1,5 +1,22 @@
 package rangeExtraction_20200531;
 
+import java.util.Stack;
+
 public class Solution {
 
+  public static String rangeExtraction(int[] arr) {
+    Stack<Integer> consecutiveStack = new Stack<>();
+    String result = "";
+    consecutiveStack.push(arr[0]);
+    for (int i = 1; i < arr.length; i++) {
+      if (consecutiveStack.peek() + 1 == arr[i]) {
+        consecutiveStack.push(arr[i]);
+      }
+    }
+    if (consecutiveStack.size() > 2) {
+      result += consecutiveStack.firstElement() + "-" + consecutiveStack.peek();
+      consecutiveStack.clear();
+    }
+    return result;
+  }
 }

7c48b1707e48e9ecdab917ba9592273f38aff114
JinHoooooou <jinho4744@naver.com>
2020-05-31 14:25:19 +0900

    2020-05-31-Range-Extraction-refactor-actual-code-for-clean-code

--- src/rangeExtraction_20200531/Solution.java
+++ src/rangeExtraction_20200531/Solution.java
@@ -3,26 +3,25 @@ package rangeExtraction_20200531;
 public class Solution {
 
   public static String rangeExtraction(int[] arr) {
-    String result = "";
-    for (int i = 0; i < arr.length; i++) {
-      int j = i + 1;
-      while (j < arr.length) {
-        if (arr[j - 1] + 1 == arr[j]) {
-          j++;
-        } else {
-          break;
-        }
+    StringBuilder result = new StringBuilder();
+    for (int startIndex = 0; startIndex < arr.length; startIndex++) {
+      result.append(arr[startIndex]);
+      int endIndex = findEndIndex(arr, startIndex);
+      int consecutiveNumberLength = endIndex - startIndex + 1;
+      if (consecutiveNumberLength >= 3) {
+        result.append("-").append(arr[endIndex]);
+        startIndex = endIndex;
       }
-      if (j - i > 2) {
-        result += arr[i] + "-" + arr[j - 1] + ",";
-        i = j - 1;
-      } else if (j - i == 2) {
-        result += arr[i] + "," + arr[j - 1] + ",";
-        i = j - 1;
-      } else {
-        result += arr[i] + ",";
-      }
+      result.append(",");
     }
     return result.substring(0, result.length() - 1);
+  }
+
+  private static int findEndIndex(int[] arr, int startIndex) {
+    int endIndex = startIndex;
+    while (endIndex < arr.length - 1 && (arr[endIndex] + 1 == arr[endIndex + 1])) {
+      endIndex++;
+    }
+    return endIndex;
   }
 }

7da9378ba41030b36f9a3bbd630b54af6f0ea6e4
JinHoooooou <jinho4744@naver.com>
2020-06-07 12:58:40 +0900

    2020-06-07-Codewars-Rectangle-Into-Square-add solution, best practice

---
+++ src/codeWars/kyu6/rectangleIntoSquare_20200607/SqInRectTest.java
@@ -0,0 +1,23 @@
+package codeWars.kyu6.rectangleIntoSquare_20200607;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class SqInRectTest {
+
+  @Test
+  @DisplayName("test should return [1,1] when input 2, 1")
+  public void test1() {
+
+    // When: Call sqInRect method
+    List<Integer> actual = SqInRect.sqInRect(2, 1);
+
+    // Then: Should return [1,1]
+    assertArrayEquals(new int[]{1, 1}, actual.stream().mapToInt(x -> x).toArray());
+  }
+}

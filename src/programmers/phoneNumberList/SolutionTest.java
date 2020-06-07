a44e5c34496437162f370671a1676ca4180a8844
JinHoooooou <jinho4744@naver.com>
2020-06-07 16:37:22 +0900

    modify test

--- src/programmers/phoneNumberList/SolutionTest.java
+++ src/programmers/phoneNumberList/SolutionTest.java
@@ -10,10 +10,30 @@ class SolutionTest {
   Solution solution = new Solution();
 
   @Test
-  @DisplayName("test should false when input [119, 97674223, 1195524421]")
+  @DisplayName("test should false when input [\"119\", \"97674223\", \"1195524421\"]")
   public void test1() {
     // Given: Set input
     String[] phoneBook = {"119", "97674223", "1195524421"};
+
+    // Then: Should false
+    assertFalse(solution.solution(phoneBook));
+  }
+
+  @Test
+  @DisplayName("test should true when input [\"123\",\"456\",\"789\"]")
+  public void test2() {
+    // Given: Set input
+    String[] phoneBook = {"123", "456", "789"};
+
+    // Then: Should true
+    assertTrue(solution.solution(phoneBook));
+  }
+
+  @Test
+  @DisplayName("test should true when input [\"12\",\"123\",\"1235\",\"567\",\"88\"]")
+  public void test3() {
+    // Given: Set input
+    String[] phoneBook = {"12", "123", "1235", "567", "88"};
 
     // Then: Should false
     assertFalse(solution.solution(phoneBook));

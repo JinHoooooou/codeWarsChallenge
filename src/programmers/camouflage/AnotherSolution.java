44fbd93a08d2200c7000e7982fbf88a0407f0081
JinHoooooou <jinho4744@naver.com>
2020-06-07 17:00:38 +0900

    2020-06-07-Programmers-위장-add-solution-and-another_solution

---
+++ src/programmers/camouflage/AnotherSolution.java
@@ -0,0 +1,24 @@
+package programmers.camouflage;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
+
+public class AnotherSolution {
+
+  public int solution(String[][] clothes) {
+    Map<String, Integer> clothesMap = new HashMap<>();
+
+    for (int i = 0; i < clothes.length; i++) {
+      String clothesType = clothes[i][1];
+      clothesMap.put(clothesType, clothesMap.getOrDefault(clothesType, 0) + 1);
+    }
+    int result = 1;
+    for (Entry<String, Integer> entry : clothesMap.entrySet()) {
+      result = result * (entry.getValue() + 1);
+    }
+    return result - 1;
+  }
+}

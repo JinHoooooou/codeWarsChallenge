c10763ce138a252105bd3cacea406154ff6d5015
JinHoooooou <jinho4744@naver.com>
2020-06-18 13:15:41 +0900

    2020-06-18-Codewars-Next-Bigger-Number-With-The-Same-Digits-add_solution_and_best_practice

---
+++ src/codeWars/kyu4/nextBiggerNumberWithTheSameDigits_20200618/KataBestPractice.java
@@ -0,0 +1,23 @@
+package codeWars.kyu4.nextBiggerNumberWithTheSameDigits_20200618;
+
+import java.util.Arrays;
+import java.util.TreeSet;
+
+public class KataBestPractice {
+
+  public static long nextBiggerNumber(long n) {
+    char[] toCharArray = String.valueOf(n).toCharArray();
+    for (int i = toCharArray.length - 2; i >= 0; i--) {
+      for (int j = toCharArray.length - 1; j > i; j--) {
+        if (toCharArray[j] > toCharArray[i]) {
+          char temp = toCharArray[i];
+          toCharArray[i] = toCharArray[j];
+          toCharArray[j] = temp;
+          Arrays.sort(toCharArray, i + 1, toCharArray.length);
+          return Long.parseLong(String.valueOf(toCharArray));
+        }
+      }
+    }
+    return -1;
+  }
+}
---
+++ src/codeWars/kyu4/nextBiggerNumberWithTheSameDigits_20200618/KataTest.java
@@ -0,0 +1,37 @@
+package codeWars.kyu4.nextBiggerNumberWithTheSameDigits_20200618;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class KataTest {
+
+  @Test
+  @DisplayName("test should return 21 when input 12")
+  public void test0() {
+    // Then: Should return 21
+    assertEquals(21, Kata.nextBiggerNumber(12));
+  }
+
+  @Test
+  @DisplayName("test should return 531 when input 513")
+  public void test1() {
+    // Then: Should return 531
+    assertEquals(531, Kata.nextBiggerNumber(513));
+  }
+
+  @Test
+  @DisplayName("test should return 2071 when input 2017")
+  public void test2() {
+    // Then: Should return 2071
+    assertEquals(2071, Kata.nextBiggerNumber(2017));
+  }
+
+  @Test
+  @DisplayName("test should return 1952808689 when input 1952806988")
+  public void test3() {
+    // Then: Should return 1952808689
+    assertEquals(1952808689, Kata.nextBiggerNumber(1952806988));
+  }
+}

f708fd7e26f9212238808a5124bf4c0a40c0db32
JinHoooooou <jinho4744@naver.com>
2020-06-18 14:34:01 +0900

    2020-06-18-Codewars-Maximum-Length-Difference-add_solution

---
+++ src/codeWars/kyu7/maximumLengthDifference_20200618/MaxDiffLengthTest.java
@@ -0,0 +1,21 @@
+package codeWars.kyu7.maximumLengthDifference_20200618;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class MaxDiffLengthTest {
+
+  @Test
+  @DisplayName("test should return 13")
+  public void test1() {
+    // Given: Set String array
+    String[] a1 = {"hoqq", "bbllkw", "oox", "ejjuyyy", "plmiis", "xxxzgpsssa", "xxwwkktt",
+        "znnnnfqknaz", "qqquuhii", "dvvvwz"};
+    String[] a2 = {"cccooommaaqqoxii", "gggqaffhhh", "tttoowwwmmww"};
+
+    // Then: Should return 13
+    assertEquals(13, MaxDiffLength.maxdiflg(a1, a2));
+  }
+}

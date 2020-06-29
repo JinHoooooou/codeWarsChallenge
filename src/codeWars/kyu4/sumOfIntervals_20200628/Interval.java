c23fd0a279a7a675632f8ba8a8335c16983df803
JinHoooooou <jinho4744@naver.com>
2020-06-29 12:21:26 +0900

    2020-06-28-Codewars-Sum-Of-Intervals-add_solution

--- src/codeWars/kyu4/sumOfIntervals_20200628/Interval.java
+++ src/codeWars/kyu4/sumOfIntervals_20200628/Interval.java
@@ -1,5 +1,20 @@
 package codeWars.kyu4.sumOfIntervals_20200628;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class Interval {
 
+  public static int sumIntervals(int[][] intervals) {
+    if (intervals == null) {
+      return 0;
+    }
+    Set<Integer> intervalSet = new HashSet<>();
+    for (int i = 0; i < intervals.length; i++) {
+      for (int j = intervals[i][0]; j < intervals[i][1]; j++) {
+        intervalSet.add(j);
+      }
+    }
+    return intervalSet.size();
+  }
 }

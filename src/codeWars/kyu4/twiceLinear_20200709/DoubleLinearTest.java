781dc0f8b8897331231a97ff0927930a05fe9b35
JinHoooooou <jinho4744@naver.com>
2020-07-09 16:59:43 +0900

    2020-07-09-Codewars-Twice-Linear-add_solution_and_best_practice

---
+++ src/codeWars/kyu4/twiceLinear_20200709/DoubleLinearTest.java
@@ -0,0 +1,17 @@
+package codeWars.kyu4.twiceLinear_20200709;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class DoubleLinearTest {
+
+  @Test
+  @DisplayName("test should return 22 when input 10")
+  public void test1() {
+    assertEquals(22, DoubleLinear.dblLinear(10));
+    assertEquals(57, DoubleLinear.dblLinear(20));
+    assertEquals(91, DoubleLinear.dblLinear(30));
+  }
+}

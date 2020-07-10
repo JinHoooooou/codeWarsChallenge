d5955221fad0350ace40c0f8fd937b68dc6e946c
JinHoooooou <jinho4744@naver.com>
2020-07-10 12:10:21 +0900

    2020-07-10-Codewars-Testing-1-2-3-add_solution

---
+++ src/codeWars/kyu7/testing1_2_3_20200710/LineNumberingTest.java
@@ -0,0 +1,17 @@
+package codeWars.kyu7.testing1_2_3_20200710;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.Arrays;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class LineNumberingTest {
+
+  @Test
+  @DisplayName("test should add index")
+  public void test1() {
+    assertEquals(
+        Arrays.asList("1: a", "2: b", "3: c"), LineNumbering.number(Arrays.asList("a", "b", "c")));
+  }
+}

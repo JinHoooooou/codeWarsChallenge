5a6956acfd35c87fc2a209e35976069d84128740
JinHoooooou <jinho4744@naver.com>
2020-07-11 14:27:05 +0900

    2020-07-11-Codewars-Pixelart-Planning-add_solution

---
+++ src/codeWars/kyu8/pixelartPlanning_20200711/Kata.java
@@ -0,0 +1,8 @@
+package codeWars.kyu8.pixelartPlanning_20200711;
+
+public class Kata {
+
+  public static boolean isDivisible(int wallLength, int pixelSize) {
+    return wallLength % pixelSize == 0;
+  }
+}

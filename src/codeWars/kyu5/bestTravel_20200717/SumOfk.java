cd1ed8f2c248b18bffb2d0fe48a2075901569f6d
JinHoooooou <jinho4744@naver.com>
2020-07-18 17:17:11 +0900

    2020-07-18-Codewars-Best-Travel-add_solution

--- src/codeWars/kyu5/bestTravel_20200717/SumOfk.java
+++ src/codeWars/kyu5/bestTravel_20200717/SumOfk.java
@@ -1,5 +1,32 @@
 package codeWars.kyu5.bestTravel_20200717;
 
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
 public class SumOfk {
 
+  static Set<Integer> sumSet;
+
+  public static Integer chooseBestSum(int limit, int townsCount, List<Integer> ls) {
+    sumSet = new HashSet<>();
+    dfs(ls, 0, limit, 0, townsCount);
+    return sumSet.size() == 0 ? null : sumSet.stream().mapToInt(x -> x).max().getAsInt();
+  }
+
+  private static void dfs(List<Integer> ls, int sum, int limit, int currentIndex, int townsCount) {
+
+    if (sum > limit) {
+      return;
+    }
+
+    if (townsCount == 0) {
+      sumSet.add(sum);
+      return;
+    }
+
+    for (int i = currentIndex; i < ls.size(); i++) {
+      dfs(ls, sum + ls.get(i), limit, i + 1, townsCount - 1);
+    }
+  }
 }
--- src/codeWars/kyu5/bestTravel_20200717/SumOfkTest.java
+++ src/codeWars/kyu5/bestTravel_20200717/SumOfkTest.java
@@ -1,5 +1,42 @@
+package codeWars.kyu5.bestTravel_20200717;
+
 import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 
 class SumOfkTest {
 
+  @Test
+  @DisplayName("test should null when k > ls.length")
+  public void test1() {
+    // Given: Set input
+    List<Integer> ls = new ArrayList<>(Arrays.asList(50));
+
+    // Then: Should null
+    assertNull(SumOfk.chooseBestSum(163, 3, ls));
+  }
+
+  @Test
+  @DisplayName("test should 163 when limit=163, townsCount=3 and list")
+  public void test2() {
+    // Given: Set input
+    List<Integer> ls = new ArrayList<>(Arrays.asList(50, 55, 56, 57, 58));
+
+    // Then: Should null
+    assertEquals(163, SumOfk.chooseBestSum(163, 3, ls));
+  }
+
+  @Test
+  @DisplayName("test should 228 when limit=230, townsCount=3, and list")
+  public void test3() {
+    // Given: Set input
+    List<Integer> ls = new ArrayList<>(Arrays.asList(91, 74, 73, 85, 73, 81, 87));
+
+    // Then: Should null
+    assertEquals(228, SumOfk.chooseBestSum(230, 3, ls));
+  }
 }

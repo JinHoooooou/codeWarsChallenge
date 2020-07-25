f0a28bdcef3aec0ac769f712018348fa07cb1cab
JinHoooooou <jinho4744@naver.com>
2020-07-25 16:15:16 +0900

    2020-07-25-Programmers-경주로건설-add_solutions

--- src/programmers/constructTrack/Solution.java
+++ src/programmers/constructTrack/Solution.java
@@ -1,5 +1,110 @@
 package programmers.constructTrack;
 
-public class Solution {
+import java.util.LinkedList;
+import java.util.Queue;
+
+class Solution {
+
+  int result = 0;
+
+  public int solution(int[][] board) {
+    int[][] costMap = new int[board.length][board.length];
+    for (int i = 0; i < board.length; i++) {
+      for (int j = 0; j < board.length; j++) {
+        costMap[i][j] = board[i][j];
+      }
+    }
+    Queue<Block> queue = new LinkedList<>();
+    queue.add(new Block(0, 0, 0, ""));
+
+    int min = Integer.MAX_VALUE;
+    while (!queue.isEmpty()) {
+      Block current = queue.poll();
+      if (current.y == costMap.length - 1 && current.x == costMap.length - 1) {
+        min = Math.min(min, current.cost);
+        continue;
+      }
+      if (current.y < 0 || current.y > costMap.length - 1) {
+        continue;
+      }
+      if (current.x < 0 || current.x > costMap.length - 1) {
+        continue;
+      }
+      if (costMap[current.y][current.x] == 1) {
+        continue;
+      }
+
+      if (costMap[current.y][current.x] == 0) {
+        costMap[current.y][current.x] = current.cost;
+        moveLeft(queue, current);
+        moveRight(queue, current);
+        moveDown(queue, current);
+        moveUp(queue, current);
+      } else if (costMap[current.y][current.x] >= current.cost) {
+        costMap[current.y][current.x] = current.cost;
+        moveLeft(queue, current);
+        moveRight(queue, current);
+        moveDown(queue, current);
+        moveUp(queue, current);
+      }
+    }
+    return min;
+  }
+
+  private void moveUp(Queue<Block> queue, Block current) {
+    int cost = 0;
+    if (current.direction.equals("up") || current.direction.equals("")) {
+      cost = current.cost + 100;
+    } else {
+      cost = current.cost + 600;
+    }
+    queue.add(new Block(current.y - 1, current.x, cost, "up"));
+  }
+
+  private void moveDown(Queue<Block> queue, Block current) {
+    int cost = 0;
+    if (current.direction.equals("down") || current.direction.equals("")) {
+      cost = current.cost + 100;
+    } else {
+      cost = current.cost + 600;
+    }
+    queue.add(new Block(current.y + 1, current.x, cost, "down"));
+  }
+
+  private void moveRight(Queue<Block> queue, Block current) {
+    int cost = 0;
+    if (current.direction.equals("right") || current.direction.equals("")) {
+      cost = current.cost + 100;
+    } else {
+      cost = current.cost + 600;
+    }
+    queue.add(new Block(current.y, current.x + 1, cost, "right"));
+
+  }
+
+  private void moveLeft(Queue<Block> queue, Block current) {
+    int cost = 0;
+    if (current.direction.equals("left") || current.direction.equals("")) {
+      cost = current.cost + 100;
+    } else {
+      cost = current.cost + 600;
+    }
+    queue.add(new Block(current.y, current.x - 1, cost, "left"));
+  }
+}
+
+class Block {
+
+  public int y;
+  public int x;
+  public int cost;
+  public String direction;
+
+  public Block(int y, int x, int cost, String direction) {
+    this.y = y;
+    this.x = x;
+    this.cost = cost;
+    this.direction = direction;
+  }
 
 }
--- src/programmers/constructTrack/SolutionTest.java
+++ src/programmers/constructTrack/SolutionTest.java
@@ -1,5 +1,118 @@
+package programmers.constructTrack;
+
 import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 
 class SolutionTest {
 
+  Solution solution = new Solution();
+  AnotherSolution anotherSolution = new AnotherSolution();
+
+  @Test
+  @DisplayName("test when 3x3 board with no block")
+  public void test1() {
+    // Given: Set board
+    int[][] board = {
+        {0, 0, 0},
+        {0, 0, 0},
+        {0, 0, 0}
+    };
+
+    assertEquals(900, solution.solution(board));
+    assertEquals(900, AnotherSolution.solution(board));
+  }
+
+  @Test
+  @DisplayName("test when 3x3 board with block")
+  public void test2() {
+    // Given: Set board
+    int[][] board = {
+        {0, 0, 1},
+        {0, 0, 0},
+        {1, 0, 0}
+    };
+
+    assertEquals(1400, solution.solution(board));
+    assertEquals(1400, AnotherSolution.solution(board));
+  }
+
+  @Test
+  @DisplayName("test when 4x4 board with no block")
+  public void test3() {
+    // Given: Set board
+    int[][] board = {
+        {0, 0, 0, 0},
+        {0, 0, 0, 0},
+        {0, 0, 0, 0},
+        {0, 0, 0, 0}
+    };
+
+    assertEquals(1100, solution.solution(board));
+    assertEquals(1100, AnotherSolution.solution(board));
+  }
+
+  @Test
+  @DisplayName("test when 4x4 board with block")
+  public void test4() {
+    // Given: Set board
+    int[][] board = {
+        {0, 0, 1, 0},
+        {1, 0, 0, 0},
+        {0, 1, 0, 1},
+        {0, 0, 0, 0}
+    };
+
+    assertEquals(2600, solution.solution(board));
+    assertEquals(2600, AnotherSolution.solution(board));
+  }
+
+  @Test
+  @DisplayName("test when 5x5 board with no block")
+  public void test5() {
+    // Given: Set board
+    int[][] board = {
+        {0, 0, 0, 0, 0},
+        {0, 0, 0, 0, 0},
+        {0, 0, 0, 0, 0},
+        {0, 0, 0, 0, 0},
+        {0, 0, 0, 0, 0}
+    };
+
+    assertEquals(1300, solution.solution(board));
+    assertEquals(1300, AnotherSolution.solution(board));
+  }
+
+  @Test
+  @DisplayName("test when 5x5 board with block")
+  public void test6() {
+    // Given: Set board
+    int[][] board = {
+        {0, 0, 0, 0, 0},
+        {0, 1, 1, 1, 0},
+        {0, 0, 1, 0, 0},
+        {1, 0, 0, 0, 1},
+        {0, 1, 0, 0, 0}
+    };
+
+    assertEquals(3000, AnotherSolution.solution(board));
+  }
+
+  @Test
+  @DisplayName("test when 6x6 board with block")
+  public void test7() {
+    // Given: Set board
+    int[][] board = {
+        {0, 0, 0, 0, 0, 0},
+        {0, 1, 1, 1, 1, 0},
+        {0, 0, 1, 0, 0, 0},
+        {1, 0, 0, 1, 0, 1},
+        {0, 1, 0, 0, 0, 1},
+        {0, 0, 0, 0, 0, 0},
+    };
+
+    assertEquals(3200, solution.solution(board));
+    assertEquals(3200, AnotherSolution.solution(board));
+  }
 }

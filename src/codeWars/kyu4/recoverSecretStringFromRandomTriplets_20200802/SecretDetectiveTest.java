8a18a8db2bd27b887a7cffd510d078a05aff353a
JinHoooooou <jinho4744@naver.com>
2020-08-02 15:50:56 +0900

    2020-08-02-Codewars-Recover-Secret-String-From-Random-Triplets-add_best_practice_as_failure_of_my_solution

---
+++ src/codeWars/kyu4/recoverSecretStringFromRandomTriplets_20200802/SecretDetectiveTest.java
@@ -0,0 +1,52 @@
+package codeWars.kyu4.recoverSecretStringFromRandomTriplets_20200802;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class SecretDetectiveTest {
+
+  SecretDetective detective = new SecretDetective();
+
+  @Test
+  public void test1() {
+    // Given: Set input
+    char[][] triplets = {
+        {'t', 'u', 'p'},
+        {'w', 'h', 'i'},
+        {'t', 's', 'u'},
+        {'a', 't', 's'},
+        {'h', 'a', 'p'},
+        {'t', 'i', 's'},
+        {'w', 'h', 's'}
+    };
+
+    assertEquals("whatisup", detective.recoverSecret(triplets));
+  }
+
+  @Test
+  public void test2() {
+    // Given: Set input
+    char[][] triplets = {
+        {'t', 's', 'f'},
+        {'a', 's', 'u'},
+        {'m', 'a', 'f'},
+        {'a', 'i', 'n'},
+        {'s', 'u', 'n'},
+        {'m', 'f', 'u'},
+        {'a', 't', 'h'},
+        {'t', 'h', 'i'},
+        {'h', 'i', 'f'},
+        {'m', 'h', 'f'},
+        {'a', 'u', 'n'},
+        {'m', 'a', 't'},
+        {'f', 'u', 'n'},
+        {'h', 's', 'n'},
+        {'a', 'i', 's'},
+        {'m', 's', 'n'},
+        {'m', 's', 'u'}
+    };
+
+    assertEquals("mathisfun", detective.recoverSecret(triplets));
+  }
+}

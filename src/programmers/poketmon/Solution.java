0b5fc8e976471736e7ed89189fa45d1185b23d7e
JinHoooooou <jinho4744@naver.com>
2020-08-04 18:13:03 +0900

    2020-08-04-Programmers-폰켓몬-add_solution

--- src/programmers/poketmon/Solution.java
+++ src/programmers/poketmon/Solution.java
@@ -1,5 +1,15 @@
 package programmers.poketmon;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class Solution {
 
+  public int solution(int[] nums) {
+    Set<Integer> set = new HashSet<>();
+    for (int num : nums) {
+      set.add(num);
+    }
+    return Math.min(set.size(), (nums.length / 2));
+  }
 }

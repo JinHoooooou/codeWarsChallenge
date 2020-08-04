c1f488e4c983f7c98f152d270fd036efb732a407
JinHoooooou <jinho4744@naver.com>
2020-08-04 18:13:34 +0900

    2020-08-04-Programmers-여행경로-add_solution

--- src/programmers/travelPath/SolutionTest.java
+++ src/programmers/travelPath/SolutionTest.java
@@ -1,5 +1,37 @@
+package programmers.travelPath;
+
 import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
 
 class SolutionTest {
 
+  Solution solution = new Solution();
+
+  @Test
+  public void test1() {
+    // Given: Set input
+    String[][] tickets = {
+        {"ICN", "JFK"},
+        {"HND", "IAD"},
+        {"JFK", "HND"}
+    };
+
+    assertArrayEquals(new String[]{"ICN", "JFK", "HND", "IAD"}, solution.solution(tickets));
+  }
+
+  @Test
+  public void test2() {
+    // Given: Set input
+    String[][] tickets = {
+        {"ICN", "SFO"},
+        {"ICN", "ATL"},
+        {"SFO", "ATL"},
+        {"ATL", "ICN"},
+        {"ATL", "SFO"}
+    };
+
+    assertArrayEquals(new String[]{"ICN", "ATL", "ICN", "SFO", "ATL", "SFO"},
+        solution.solution(tickets));
+  }
 }

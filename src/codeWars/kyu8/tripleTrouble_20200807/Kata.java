2b62047023b898009fd31e415ff958fe87cd3417
JinHoooooou <jinho4744@naver.com>
2020-08-07 22:19:42 +0900

    2020-08-07-Codewars-Triple-Trouble-add_solution

--- src/codeWars/kyu8/tripleTrouble_20200807/Kata.java
+++ src/codeWars/kyu8/tripleTrouble_20200807/Kata.java
@@ -2,4 +2,11 @@ package codeWars.kyu8.tripleTrouble_20200807;
 
 public class Kata {
 
+  public static String tripleTrouble(String one, String two, String three) {
+    StringBuilder result = new StringBuilder();
+    for (int i = 0; i < one.length(); i++) {
+      result.append(one.charAt(i)).append(two.charAt(i)).append(three.charAt(i));
+    }
+    return result.toString();
+  }
 }

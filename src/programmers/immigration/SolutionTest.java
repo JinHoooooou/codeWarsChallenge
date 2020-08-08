d691717dd79fef7f188f4604084e7958d7fa45b7
JinHoooooou <jinho4744@naver.com>
2020-08-08 16:39:30 +0900

    2020-08-08-Programmers-입국심사-add_solution

---
+++ src/programmers/immigration/SolutionTest.java
@@ -0,0 +1,18 @@
+package programmers.immigration;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class SolutionTest {
+
+  Solution solution = new Solution();
+
+  @Test
+  public void test1() {
+    int[] times = {7, 10};
+    int n = 6;
+
+    assertEquals(28, solution.solution(n, times));
+  }
+}

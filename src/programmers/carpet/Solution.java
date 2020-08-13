df86365f66e3900a4f81fbd46ef13e8319db0484
JinHoooooou <jinho4744@naver.com>
2020-08-13 15:44:05 +0900

    2020-08-13-Programmers-카펫-add_solution

--- src/programmers/carpet/Solution.java
+++ src/programmers/carpet/Solution.java
@@ -1,5 +1,28 @@
 package programmers.carpet;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Solution {
 
+  public int[] solution(int brown, int yellow) {
+    int area = brown + yellow;
+    List<Integer> horizonList = new ArrayList<>();
+    for (int i = 1; i <= area / 2; i++) {
+      if (area % i == 0) {
+        int horizon = i;
+        int vertical = area / i;
+        if (horizon >= vertical) {
+          horizonList.add(horizon);
+        }
+      }
+    }
+
+    for (Integer i : horizonList) {
+      if (area - ((i - 2) * ((area / i) - 2)) == brown) {
+        return new int[]{i, area / i};
+      }
+    }
+    return new int[]{0, 0};
+  }
 }
--- src/programmers/carpet/SolutionTest.java
+++ src/programmers/carpet/SolutionTest.java
@@ -1,5 +1,15 @@
+package programmers.carpet;
+
 import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
 
 class SolutionTest {
 
+  Solution solution = new Solution();
+
+  @Test
+  public void test1() {
+    assertArrayEquals(new int[]{8, 6}, solution.solution(24, 24));
+  }
 }

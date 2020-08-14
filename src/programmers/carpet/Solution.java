0f9f8043756d8ff69843374b32a86039945f680e
JinHoooooou <jinho4744@naver.com>
2020-08-14 14:48:13 +0900

    refactor for clean code

--- src/programmers/carpet/Solution.java
+++ src/programmers/carpet/Solution.java
@@ -7,22 +7,16 @@ public class Solution {
 
   public int[] solution(int brown, int yellow) {
     int area = brown + yellow;
-    List<Integer> horizonList = new ArrayList<>();
-    for (int i = 1; i <= area / 2; i++) {
-      if (area % i == 0) {
-        int horizon = i;
-        int vertical = area / i;
-        if (horizon >= vertical) {
-          horizonList.add(horizon);
+
+    for (int vertical = 1; vertical <= area / 2; vertical++) {
+      if (area % vertical == 0) {
+        int horizon = area / vertical;
+        if ((horizon - 2) * (vertical - 2) == yellow) {
+          return new int[]{horizon, vertical};
         }
       }
     }
 
-    for (Integer i : horizonList) {
-      if (area - ((i - 2) * ((area / i) - 2)) == brown) {
-        return new int[]{i, area / i};
-      }
-    }
     return new int[]{0, 0};
   }
 }

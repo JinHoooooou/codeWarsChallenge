3ca83d87d863dc931c68c7ef1e346e7afdd549ac
JinHoooooou <jinho4744@naver.com>
2020-08-15 22:56:19 +0900

    2020-08-15-Programmers-후보키-add_solution

---
+++ src/programmers/candidateKey/SolutionTest.java
@@ -0,0 +1,25 @@
+package programmers.candidateKey;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class SolutionTest {
+
+  Solution solution = new Solution();
+
+  @Test
+  public void test1() {
+    // Given: Set relation
+    String[][] relation = {
+        {"100", "ryan", "music", "2"},
+        {"200", "apeach", "math", "2"},
+        {"300", "tube", "computer", "3"},
+        {"400", "con", "computer", "4"},
+        {"500", "muzi", "music", "3"},
+        {"600", "apeach", "music", "2"},
+    };
+
+    assertEquals(2, solution.solution(relation));
+  }
+}

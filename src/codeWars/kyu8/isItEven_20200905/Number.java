fa69f6255e9bbc44850a93ce948207e63169aba7
JinHoooooou <jinho4744@naver.com>
2020-09-06 14:03:21 +0900

    2020-09-05-Codewars-Is-It-Even-add_solution

--- src/codeWars/kyu8/isItEven_20200905/Number.java
+++ src/codeWars/kyu8/isItEven_20200905/Number.java
@@ -2,4 +2,7 @@ package codeWars.kyu8.isItEven_20200905;
 
 public class Number {
 
+  public boolean isEven(double n) {
+    return n % 2 == 0;
+  }
 }
--- src/codeWars/kyu8/isItEven_20200905/NumberTest.java
+++ src/codeWars/kyu8/isItEven_20200905/NumberTest.java
@@ -1,5 +1,30 @@
+package codeWars.kyu8.isItEven_20200905;
+
 import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
 
 class NumberTest {
 
+  Number number = new Number();
+
+  @Test
+  public void testWhenInputIs1() {
+    assertFalse(number.isEven(1));
+  }
+
+  @Test
+  public void testWhenInputIs4() {
+    assertTrue(number.isEven(4));
+  }
+
+  @Test
+  public void testWhenInputIsNegative() {
+    assertTrue(number.isEven(-64));
+  }
+
+  @Test
+  public void testWhenInputIsFloat() {
+    assertFalse(number.isEven(2.2468));
+  }
 }

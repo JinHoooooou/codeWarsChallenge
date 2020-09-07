8d55dc0144f5705ff3098cc27ce5655ab7d99734
JinHoooooou <jinho4744@naver.com>
2020-09-07 15:48:25 +0900

    2020-09-07-Codewars-Sum-Of-Parts-add_solution

---
+++ src/codeWars/kyu6/SumOfParts_20209007/SumParts.java
@@ -0,0 +1,14 @@
+package codeWars.kyu6.SumOfParts_20209007;
+
+import java.util.Arrays;
+
+public class SumParts {
+
+  public static int[] sumParts(int[] ls) {
+    int[] result = new int[ls.length + 1];
+    for (int i = ls.length - 1; i >= 0; i--) {
+      result[i] = result[i + 1] + ls[i];
+    }
+    return result;
+  }
+}
---
+++ src/codeWars/kyu6/SumOfParts_20209007/SumPartsTest.java
@@ -0,0 +1,27 @@
+package codeWars.kyu6.SumOfParts_20209007;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class SumPartsTest {
+
+  @Test
+  public void testWhenInputLength0() {
+    assertArrayEquals(new int[]{0}, SumParts.sumParts(new int[]{}));
+  }
+
+  @Test
+  public void testWhenInputLength1() {
+    assertArrayEquals(new int[]{1, 0}, SumParts.sumParts(new int[]{1}));
+  }
+
+  @Test
+  public void testWhenInputIsComposedOfLargeNumber() {
+    int[] input = {744125, 935, 407, 454, 430, 90, 144, 6710213, 889, 810, 2579358};
+    int[] expected = {10037855, 9293730, 9292795, 9292388, 9291934, 9291504, 9291414, 9291270,
+        2581057, 2580168, 2579358, 0};
+
+    assertArrayEquals(expected, SumParts.sumParts(input));
+  }
+}

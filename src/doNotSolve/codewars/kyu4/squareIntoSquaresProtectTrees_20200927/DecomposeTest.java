8260ee29e854336bc1bfd1a078d19f914793fd98
JinHoooooou <jinho4744@naver.com>
2020-09-28 13:08:53 +0900

    2020-09-28-DoNotSolve(Codewars)-Square-Into-Squares-Protect-Trees-add_solution_after_watching_solution

---
+++ src/doNotSolve/codewars/kyu4/squareIntoSquaresProtectTrees_20200927/DecomposeTest.java
@@ -0,0 +1,23 @@
+package doNotSolve.codewars.kyu4.squareIntoSquaresProtectTrees_20200927;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class DecomposeTest {
+
+  @Test
+  public void testWhenInputIs1() {
+    assertEquals(null, new Decompose().decompose(1));
+  }
+
+  @Test
+  public void testWhenInputIs11() {
+    assertEquals("1 2 4 10", new Decompose().decompose(11));
+  }
+
+  @Test
+  public void testWhenInputIs12() {
+    assertEquals("1 2 3 7 9", new Decompose().decompose(12));
+  }
+}

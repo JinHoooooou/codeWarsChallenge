79fed31968c89f4e7fddb96d1249810fc5d517d5
JinHoooooou <jinho4744@naver.com>
2020-10-07 14:07:49 +0900

    2020-10-07-Programmers-2016년-add_solution

---
+++ src/programmers/_2016년/Solution.java
@@ -0,0 +1,18 @@
+package programmers._2016년;
+
+import java.util.Calendar;
+
+public class Solution {
+
+  public static String solution(int a, int b) {
+    int[] month = {30, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+    String[] weekOfDay = {"FRI", "SAT", "SUN", "MON", "TUE", "WED", "THU"};
+    int day = b;
+
+    for (int i = 0; i < a - 1; i++) {
+      day += month[i];
+    }
+
+    return weekOfDay[day % 7];
+  }
+}

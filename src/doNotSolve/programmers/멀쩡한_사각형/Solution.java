bc2e11081ff875aff3136a3dc1629c3c588e9bed
JinHoooooou <jinho4744@naver.com>
2020-10-08 17:19:35 +0900

    2020-10-08-DoNotSolve(Programmers)-멀쩡한-사각형-add_solution_after_watching_solution

---
+++ src/doNotSolve/programmers/멀쩡한_사각형/Solution.java
@@ -0,0 +1,21 @@
+package doNotSolve.programmers.멀쩡한_사각형;
+
+public class Solution {
+
+  public long solution(int width, int height) {
+    long gcd = gcd((long) width, (long) height);
+    long gcdWidth = (long) width / gcd;
+    long gcdHeight = (long) height / gcd;
+
+    long tornSquareCount = (gcdWidth + gcdHeight - 1) * gcd;
+
+    return ((long) width * (long) height - tornSquareCount);
+  }
+
+  private long gcd(long width, long height) {
+    if (height == 0) {
+      return width;
+    }
+    return gcd(height, width % height);
+  }
+}

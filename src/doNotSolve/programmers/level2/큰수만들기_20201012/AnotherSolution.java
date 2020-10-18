a90406a45b466c31da0e39679a10157ad7a147dc
JinHoooooou <jinho4744@naver.com>
2020-10-18 12:04:06 +0900

    2020-10-18-DoNotSolve(Programmers)-큰수만들기-modify_best_practice

--- src/doNotSolve/programmers/level2/큰수만들기_20201012/AnotherSolution.java
+++ src/doNotSolve/programmers/level2/큰수만들기_20201012/AnotherSolution.java
@@ -1,19 +1,22 @@
 package doNotSolve.programmers.level2.큰수만들기_20201012;
 
 import java.util.Stack;
+import java.util.stream.Collectors;
 
 public class AnotherSolution {
 
   public String solution(String number, int k) {
-    Stack<String> digits = new Stack<>();
+    Stack<Character> digits = new Stack<>();
+    int exceptCount = 0;
     for (int i = 0; i < number.length(); i++) {
-      String digit = number.charAt(i) + "";
-      while (!digits.isEmpty() && digits.peek().compareTo(digit) < 0 && k-- > 0) {
+      char digit = number.charAt(i);
+      while (!digits.isEmpty() && digits.peek() < digit && exceptCount < k) {
+        exceptCount++;
         digits.pop();
       }
       digits.push(digit);
     }
-    String[] result = digits.toArray(new String[0]);
-    return String.join("", result);
+    return digits.stream().map(String::valueOf).collect(Collectors.joining())
+        .substring(0, number.length() - k);
   }
 }

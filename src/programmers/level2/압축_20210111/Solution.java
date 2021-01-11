a3e3472ad549e5557777b702b17fcec1db76251d
JinHoooooou <jinho4744@naver.com>
2021-01-11 13:08:10 +0900

    2021-01-11-Programmers-압축-add_solution

---
+++ src/programmers/level2/압축_20210111/Solution.java
@@ -0,0 +1,51 @@
+package programmers.level2.압축_20210111;
+
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class Solution {
+
+  public int[] solution(String msg) {
+    Map<String, Integer> dictionary = buildBasicDictionary();
+    List<Integer> indexList = new ArrayList<>();
+    int letterIndex = 27;
+    int startIndex = 0;
+
+    while (startIndex < msg.length()) {
+      int endIndex = getEndIndex(msg, dictionary, startIndex);
+      String target = msg.substring(startIndex, endIndex - 1);
+
+      indexList.add(dictionary.get(target));
+      if (endIndex < msg.length()) {
+        dictionary.put(msg.substring(startIndex, endIndex), letterIndex++);
+      }
+
+      startIndex = endIndex - 1;
+    }
+
+    return indexList.stream().mapToInt(x -> x).toArray();
+  }
+
+  private int getEndIndex(String msg, Map<String, Integer> dictionary, int startIndex) {
+    int endIndex;
+    for (endIndex = startIndex + 1; endIndex <= msg.length(); endIndex++) {
+      if (!dictionary.containsKey(msg.substring(startIndex, endIndex))) {
+        break;
+      }
+    }
+    return endIndex;
+  }
+
+  private Map<String, Integer> buildBasicDictionary() {
+    Map<String, Integer> map = new HashMap<>();
+    int index = 1;
+    for (char letter = 'A'; letter <= 'Z'; letter++, index++) {
+      map.put(letter + "", index);
+    }
+    return map;
+  }
+}
+

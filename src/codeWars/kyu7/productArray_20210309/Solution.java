09beecb86fd33864d9b089015e58e015ffcdafe4
JinHoooooou <jinho4744@naver.com>
2021-03-09 10:30:16 +0900

    2021-03-09-Codewars-Product-Array-add_solution

---
+++ src/codeWars/kyu7/productArray_20210309/Solution.java
@@ -0,0 +1,22 @@
+package codeWars.kyu7.productArray_20210309;
+
+import java.math.BigInteger;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Solution {
+
+  public static long[] productArray(int[] numbers) {
+    List<Long> result = new ArrayList<>();
+    BigInteger product = BigInteger.ONE;
+    for (int i = 0; i < numbers.length; i++) {
+      product = product.multiply(BigInteger.valueOf(numbers[i]));
+    }
+
+    for (int i = 0; i < numbers.length; i++) {
+      result.add(product.divide(BigInteger.valueOf(numbers[i])).longValue());
+    }
+    return result.stream().mapToLong(x -> x).toArray();
+  }
+}

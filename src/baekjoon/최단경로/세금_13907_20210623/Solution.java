4e86ebd39817582cb88b6bca09ada7df1578260e
JinHoooooou <jinho4744@naver.com>
2021-06-24 16:40:59 +0900

    2021-06-24-백준-세금-13907-modify_to_right_solution

--- src/baekjoon/최단경로/세금_13907_20210623/Solution.java
+++ src/baekjoon/최단경로/세금_13907_20210623/Solution.java
@@ -19,17 +19,40 @@ public class Solution {
       new OutputStreamWriter(System.out));
   private static final int INF = Integer.MAX_VALUE - 1000000;
   private static StringTokenizer tokenizer;
+  private static int vertexCount;
+  private static int edgeCount;
 
   public static void main(String[] args) throws IOException {
     tokenizer = new StringTokenizer(READER.readLine());
-    int vertexCount = Integer.parseInt(tokenizer.nextToken());
-    int edgeCount = Integer.parseInt(tokenizer.nextToken());
+    vertexCount = Integer.parseInt(tokenizer.nextToken());
+    edgeCount = Integer.parseInt(tokenizer.nextToken());
     int taxCount = Integer.parseInt(tokenizer.nextToken());
 
     tokenizer = new StringTokenizer(READER.readLine());
     int source = Integer.parseInt(tokenizer.nextToken());
     int destination = Integer.parseInt(tokenizer.nextToken());
 
+    List<int[]>[] edges = buildEdges();
+    long[][] distance = dijkstra(edges, source, destination);
+
+    int tax = 0;
+    while (taxCount-- > 0) {
+      tokenizer = new StringTokenizer(READER.readLine());
+      tax += Integer.parseInt(tokenizer.nextToken());
+      long min = INF;
+
+      for (int i = 0; i < vertexCount; i++) {
+        min = Math.min(min, distance[i][destination] + (i * tax));
+      }
+      WRITER.write(min + "\n");
+    }
+
+    WRITER.flush();
+    WRITER.close();
+    READER.close();
+  }
+
+  private static List<int[]>[] buildEdges() throws IOException {
     List<int[]>[] edges = new List[vertexCount + 1];
     for (int i = 1; i <= vertexCount; i++) {
       edges[i] = new ArrayList<>();
@@ -44,67 +67,54 @@ public class Solution {
       edges[from].add(new int[]{to, weight});
       edges[to].add(new int[]{from, weight});
     }
+    return edges;
+  }
 
-    PriorityQueue<int[]> queue = new PriorityQueue<>(Comparator.comparingInt(o -> o[1]));
-    int[][] distance = new int[vertexCount][vertexCount + 1];
+  private static long[][] dijkstra(List<int[]>[] edges, int source, int destination)
+      throws IOException {
+    long[][] distance = new long[vertexCount][vertexCount + 1];
+    PriorityQueue<long[]> queue = new PriorityQueue<>(Comparator.comparingLong(x -> x[1]));
     for (int i = 0; i < vertexCount; i++) {
       Arrays.fill(distance[i], INF);
     }
-    queue.add(new int[]{source, 0, 0});
-    for (int i = 0; i < vertexCount; i++) {
-      distance[i][source] = 0;
-    }
+    queue.add(new long[]{source, 0, 0});
+    distance[0][source] = 0;
+    long min = INF;
 
     while (!queue.isEmpty()) {
-      int[] current = queue.poll();
-      int vertex = current[0];
-      int stopOver = current[2];
+      long[] current = queue.poll();
+      int vertex = (int) current[0];
+      long minDistance = current[1];
+      int stopOver = (int) current[2];
 
-      if (distance[stopOver][vertex] == INF) {
+      boolean check = false;
+      for (int i = 0; i < stopOver; i++) {
+        if (distance[i][vertex] < minDistance) {
+          check = true;
+          break;
+        }
+      }
+
+      if (check || distance[stopOver][vertex] < minDistance) {
         continue;
       }
-      if (stopOver == vertexCount - 1) {
-        continue;
+      if (vertex == destination) {
+        min = Math.min(min, distance[stopOver][vertex]);
       }
 
       for (int[] adjEdge : edges[vertex]) {
         int to = adjEdge[0];
         int weight = adjEdge[1];
 
-        int min = INF;
-        for (int i = 0; i < vertexCount; i++) {
-          min = Math.min(min, distance[i][to]);
-        }
-
-        if (min > distance[stopOver][vertex] + weight) {
+        if (stopOver + 1 < vertexCount
+            && distance[stopOver + 1][to] > distance[stopOver][vertex] + weight) {
           distance[stopOver + 1][to] = distance[stopOver][vertex] + weight;
-          queue.add(new int[]{to, distance[stopOver + 1][to], stopOver + 1});
-        } else {
-          distance[stopOver + 1][to] = min;
+          queue.add(new long[]{to, distance[stopOver + 1][to], stopOver + 1});
         }
       }
     }
 
-    int min = INF;
-    for (int i = 0; i < vertexCount; i++) {
-      min = Math.min(min, distance[i][destination]);
-    }
     WRITER.write(min + "\n");
-
-    while (taxCount-- > 0) {
-      tokenizer = new StringTokenizer(READER.readLine());
-      int tax = Integer.parseInt(tokenizer.nextToken());
-      min = distance[0][destination];
-
-      for (int i = 0; i < vertexCount; i++) {
-        distance[i][destination] += (i * tax);
-        min = Math.min(min, distance[i][destination]);
-      }
-      WRITER.write(min + "\n");
-    }
-
-    WRITER.flush();
-    WRITER.close();
-    READER.close();
+    return distance;
   }
 }

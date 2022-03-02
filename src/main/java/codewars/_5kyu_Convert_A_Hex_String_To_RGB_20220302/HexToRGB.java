34fa2211d5f60b0d77944e1fbb46a743072d092e
JINHO LEE <jinho4744@naver.com>
2022-03-02 10:35:29 +0900

    2022-03-02, Codewars-5kyu, Convert A Hex String To RGB, add my solution and best practice, success

---
+++ src/main/java/codewars/_5kyu_Convert_A_Hex_String_To_RGB_20220302/HexToRGB.java
@@ -0,0 +1,27 @@
+package codewars._5kyu_Convert_A_Hex_String_To_RGB_20220302;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class HexToRGB {
+
+  public static int[] hexStringToRGB(String hex) {
+    List<Integer> result = new ArrayList<>();
+    String token = hex.substring(1);
+    while (!token.isEmpty()) {
+      String color = token.substring(0, 2);
+      int rgb = Integer.parseInt(color, 16);
+      result.add(rgb);
+      token = token.substring(2);
+    }
+    return result.stream().mapToInt(x -> x).toArray();
+  }
+
+  public static int[] hexStringToRGBBestPractice(String hex) {
+    return new int[]{
+        Integer.parseInt(hex.substring(1, 3), 16),
+        Integer.parseInt(hex.substring(3, 5), 16),
+        Integer.parseInt(hex.substring(5, 7), 16)
+    };
+  }
+}
---
+++ src/test/java/codewars/_5kyu_Convert_A_Hex_String_To_RGB_20220302/HexToRGBTest.java
@@ -0,0 +1,45 @@
+package codewars._5kyu_Convert_A_Hex_String_To_RGB_20220302;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class HexToRGBTest {
+
+  @Test
+  public void test1() {
+    String hex = "#000000";
+    int[] expected = {0, 0, 0};
+    assertThat(HexToRGB.hexStringToRGB(hex), is(expected));
+  }
+
+  @Test
+  public void test2() {
+    String hex = "#FF9933";
+    int[] expected = {255, 153, 51};
+    assertThat(HexToRGB.hexStringToRGB(hex), is(expected));
+  }
+
+  @Test
+  public void test3() {
+    String hex = "#111111";
+    int[] expected = {17, 17, 17};
+    assertThat(HexToRGB.hexStringToRGB(hex), is(expected));
+  }
+
+  @Test
+  public void test4() {
+    String hex = "#beaded";
+    int[] expected = {190, 173, 237};
+    assertThat(HexToRGB.hexStringToRGB(hex), is(expected));
+  }
+
+  @Test
+  public void test5() {
+    String hex = "#Fa3456";
+    int[] expected = {250, 52, 86};
+    assertThat(HexToRGB.hexStringToRGB(hex), is(expected));
+  }
+}
